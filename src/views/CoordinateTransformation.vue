966d1d8e905d7f69c6a8862fb29261ec5607afd5
Your Name <you@example.com>
2023-03-02 13:45:01 +0800

    cesium坐标系转换

--- src/views/CoordinateTransformation.vue
+++ src/views/CoordinateTransformation.vue
@@ -39,8 +39,24 @@ onMounted(() => {
   // 隐藏logo
   viewer.cesiumWidget.creditContainer.style.display = "none";
 
-  // 二维屏幕坐标系
-  
+  // 二维屏幕坐标系，二维笛卡尔坐标系 Cartesian2
+  // 地理坐标系 wgs-84坐标系 Cartographic类型，通过经纬度，高度
+  // 笛卡尔空间直角坐标系 Cartesian3类型
+
+  // 角度转弧度
+  const radians = Cesium.Math.toRadians(90);
+  console.log("角度转弧度", radians);
+  // 弧度转角度
+  const degrees = Cesium.Math.toDegrees(2 * Math.PI);
+  console.log("弧度转角度", degrees);
+
+  // 经纬度转笛卡尔坐标[参数：经纬度，高度]
+  const Cartesian3 = Cesium.Cartesian3.fromDegrees(89.5, 20.4, 10);
+  console.log("经纬度转笛卡尔坐标[参数：经纬度，高度]", Cartesian3);
+
+  // 笛卡尔转经纬度
+  const cartographic = Cesium.Cartographic.fromCartesian(Cartesian3);
+  console.log("笛卡尔转经纬度", cartographic);
 });
 </script>
 
